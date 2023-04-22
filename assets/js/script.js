52c0482d5d740ed2685e0475f0c907b3ae809ef1
LO$R <106779656+madhead341@users.noreply.github.com>
2023-04-22 10:58:12 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,30 +23,28 @@ const songs = [
 ];
 
 
-// Get a random song from the array
 var randomIndex = Math.floor(Math.random() * songs.length);
 var randomSong = songs[randomIndex];
 
-// Create a new audio element and set its source to the random song
 var audio = document.getElementById('audio');
 audio.src = randomSong;
 
-// Play the audio when it's loaded and loop it
-audio.oncanplay = function() {
+document.addEventListener("click", function() {
   audio.play();
   audio.loop = true;
-};
+});
 
-// Play the next song when the current one ends
 audio.onended = function() {
-  // Get a new random song from the array
+  
   var newIndex = Math.floor(Math.random() * songs.length);
   var newSong = songs[newIndex];
-  // Set the source of the audio element to the new song
+  
   audio.src = newSong;
-  // Play the audio when it's loaded and loop it
+  
   audio.oncanplay = function() {
-    audio.play();
-    audio.loop = true;
+    document.addEventListener("click", function() {
+      audio.play();
+      audio.loop = true;
+    });
   };
 };
