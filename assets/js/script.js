2d4c0d9aaf0431d6e3824d599b3bf13abc2c7d79
LO$R <106779656+madhead341@users.noreply.github.com>
2023-04-22 16:52:52 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,43 +3,58 @@ var loadingScreen = document.querySelector(".loading-screen");
 siteWrapper.style.filter = "blur(10px)";
 loadingScreen.style.display = "flex";
 
-
 var customCursor = document.createElement("div");
 customCursor.classList.add("custom-cursor");
 document.body.appendChild(customCursor);
 
+var songs = [
+  "Cant Trust - Keef Chief.mp3",
+  "Control - Ab-Soul.mp3",
+  "DNA - Kendrick Lamar.mp3",
+  "Everyday - Chief Keef.mp3",
+  "Geeked (Bass Boosted) - CHII WVTTZ + Set Da Trend + C Blu + Dougie B.mp3",
+  "Get Out - Comethazine.mp3",
+  "Go Stupid - Polo G.mp3",
+  "I Don't Like - Chief Keef.mp3",
+  "In Control - Youngboy Never Broke Again.mp3",
+  "Love Sosa - Chief Keef.mp3",
+  "M.A.A.D. City - Kendrick Lamar.mp3",
+  "Make No Sense - Youngboy Never Broke Again.mp3",
+  "Monster - Kanye West.mp3",
+  "Only You Freestyle - Headie One.mp3",
+  "Sawarim - idk the author.mp3",
+  "Sosa - Chief Keef.mp3",
+  "Spinback - Comethazine.mp3",
+  "Still Ballin' (Remix) - 2Pac.mp3",
+  "True Religion Fein - Chief Keef.mp3",
+  "Walk - Comethanize.mp3",
+  "Wesley's Theory - Kendrick Lamar.mp3"
+];
+
+var path = "assets/music/";
+var currentSongIndex = Math.floor(Math.random() * songs.length);
+var audio = new Audio(path + songs[currentSongIndex]);
+audio.loop = false;
+audio.preload = "auto";
+
+changeTitle();
+
+audio.addEventListener("ended", function() {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  audio.src = path + songs[currentSongIndex];
+  audio.play();
+  changeTitle();
+});
+
+function changeTitle() {
+  var songName = songs[currentSongIndex].replace(".mp3", "");
+  document.title = songName + " - My Music Player";
+}
 
 document.addEventListener("mousemove", function(event) {
   customCursor.style.left = event.clientX + "px";
   customCursor.style.top = event.clientY + "px";
 });
 
-var songs = [
-  "assets/music/Cant Trust - Keef Chief.mp3",
-  "assets/music/Control - Ab-Soul.mp3",
-  "assets/music/DNA - Kendrick Lamar.mp3",
-  "assets/music/Everyday - Chief Keef.mp3",
-  "assets/music/Geeked (Bass Boosted) - CHII WVTTZ + Set Da Trend + C Blu + Dougie B.mp3",
-  "assets/music/Get Out - Comethazine.mp3",
-  "assets/music/Go Stupid - Polo G.mp3",
-  "assets/music/I Don't Like - Chief Keef.mp3",
-  "assets/music/In Control - Youngboy Never Broke Again.mp3",
-  "assets/music/Love Sosa - Chief Keef.mp3",
-  "assets/music/M.A.A.D. City - Kendrick Lamar.mp3",
-  "assets/music/Make No Sense - Youngboy Never Broke Again.mp3",
-  "assets/music/Monster - Kanye West.mp3",
-  "assets/music/Only You Freestyle - Headie One.mp3",
-  "assets/music/Sawarim - idk the author.mp3",
-  "assets/music/Sosa - Chief Keef.mp3",
-  "assets/music/Spinback - Comethazine.mp3",
-  "assets/music/Still Ballin' (Remix) - 2Pac.mp3",
-  "assets/music/True Religion Fein - Chief Keef.mp3",
-  "assets/music/Walk - Comethanize.mp3",
-  "assets/music/Wesley's Theory - Kendrick Lamar.mp3",
-]
-var random_song = songs[Math.floor(Math.random() * songs.length)];
-var path = "/assets/music/" + random_song;
-var audio = new Audio(path);
-audio.loop = true;
-audio.preload = "auto";
-changeTitle();
+siteWrapper.style.filter = "none";
+loadingScreen.style.display = "none";
