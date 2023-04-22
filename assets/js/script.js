59180480ab33a40facdd4f6d0ededc2a70722d84
LO$R <106779656+madhead341@users.noreply.github.com>
2023-04-22 18:34:17 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,11 @@ var customCursor = document.createElement("div");
 customCursor.classList.add("custom-cursor");
 document.body.appendChild(customCursor);
 
-// Update cursor position on mousemove
 document.addEventListener("mousemove", function(event) {
   customCursor.style.left = event.clientX + "px";
   customCursor.style.top = event.clientY + "px";
 });
 
-// Update cursor position on scroll
 document.addEventListener("scroll", function() {
   var scrollX = window.scrollX || window.pageXOffset;
   var scrollY = window.scrollY || window.pageYOffset;
@@ -16,36 +14,55 @@ document.addEventListener("scroll", function() {
   customCursor.style.top = event.clientY + scrollY + "px";
 });
 
-var songs = [
-  "assets/music/Cant Trust - Keef Chief.mp3",
-  "assets/music/Control - Ab-Soul.mp3",
-  "assets/music/DNA - Kendrick Lamar.mp3",
-  "assets/music/Everyday - Chief Keef.mp3",
-  "assets/music/Geeked (Bass Boosted) - CHII WVTTZ + Set Da Trend + C Blu + Dougie B.mp3",
-  "assets/music/Get Out - Comethazine.mp3",
-  "assets/music/Go Stupid - Polo G.mp3",
-  "assets/music/I Don't Like - Chief Keef.mp3",
-  "assets/music/In Control - Youngboy Never Broke Again.mp3",
-  "assets/music/Love Sosa - Chief Keef.mp3",
-  "assets/music/M.A.A.D. City - Kendrick Lamar.mp3",
-  "assets/music/Make No Sense - Youngboy Never Broke Again.mp3",
-  "assets/music/Monster - Kanye West.mp3",
-  "assets/music/Only You Freestyle - Headie One.mp3",
-  "assets/music/Sawarim - idk the author.mp3",
-  "assets/music/Sosa - Chief Keef.mp3",
-  "assets/music/Spinback - Comethazine.mp3",
-  "assets/music/Still Ballin' (Remix) - 2Pac.mp3",
-  "assets/music/True Religion Fein - Chief Keef.mp3",
-  "assets/music/Walk - Comethanize.mp3",
-  "assets/music/Wesley's Theory - Kendrick Lamar.mp3",
-];
-
-var random_song = songs[Math.floor(Math.random() * songs.length)];
-var audio = new Audio(random_song);
-audio.loop = true;
-audio.preload = "auto";
-audio.play();
-changeTitle();
+window.onload = function() {
+  var blackScreen = document.getElementById("black-screen");
+  
+  blackScreen.addEventListener("click", function() {
+    blackScreen.style.display = "none";
+    
+    var songs = [
+      "assets/music/Cant Trust - Keef Chief.mp3",
+      "assets/music/Control - Ab-Soul.mp3",
+      "assets/music/DNA - Kendrick Lamar.mp3",
+      "assets/music/Everyday - Chief Keef.mp3",
+      "assets/music/Geeked (Bass Boosted) - CHII WVTTZ + Set Da Trend + C Blu + Dougie B.mp3",
+      "assets/music/Get Out - Comethazine.mp3",
+      "assets/music/Go Stupid - Polo G.mp3",
+      "assets/music/I Don't Like - Chief Keef.mp3",
+      "assets/music/In Control - Youngboy Never Broke Again.mp3",
+      "assets/music/Love Sosa - Chief Keef.mp3",
+      "assets/music/M.A.A.D. City - Kendrick Lamar.mp3",
+      "assets/music/Make No Sense - Youngboy Never Broke Again.mp3",
+      "assets/music/Monster - Kanye West.mp3",
+      "assets/music/Only You Freestyle - Headie One.mp3",
+      "assets/music/Sawarim - idk the author.mp3",
+      "assets/music/Sosa - Chief Keef.mp3",
+      "assets/music/Spinback - Comethazine.mp3",
+      "assets/music/Still Ballin' (Remix) - 2Pac.mp3",
+      "assets/music/True Religion Fein - Chief Keef.mp3",
+      "assets/music/Walk - Comethanize.mp3",
+      "assets/music/Wesley's Theory - Kendrick Lamar.mp3"
+    ];
+    var audio = new Audio();
+    audio.loop = true;
+    audio.preload = "auto";
+    var currentSongIndex = -1;
+    
+    function playNextSong() {
+      var nextSongIndex = Math.floor(Math.random() * songs.length);
+      while (nextSongIndex === currentSongIndex) {
+        nextSongIndex = Math.floor(Math.random() * songs.length);
+      }
+      currentSongIndex = nextSongIndex;
+      var nextSong = songs[currentSongIndex];
+      audio.src = nextSong;
+      audio.play();
+    }
+    
+    audio.addEventListener("ended", playNextSong);
+    playNextSong();
+  });
+};
 
 window.addEventListener("load", function() {
   audio.play();
