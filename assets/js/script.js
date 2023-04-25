9e669e270cc5eabbd41ea745a8b0a96fe7fd7a3b
LO$R <106779656+madhead341@users.noreply.github.com>
2023-04-25 18:23:13 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,57 +54,61 @@ document.addEventListener("scroll", function() {
 });
 
     
-    var songs = [
-      "assets/music/Cant Trust - Keef Chief.mp3",
-      "assets/music/Control - Ab-Soul.mp3",
-      "assets/music/DNA - Kendrick Lamar.mp3",
-      "assets/music/Everyday - Chief Keef.mp3",
-      "assets/music/Geeked (Bass Boosted) - CHII WVTTZ + Set Da Trend + C Blu + Dougie B.mp3",
-      "assets/music/Get Out - Comethazine.mp3",
-      "assets/music/Go Stupid - Polo G.mp3",
-      "assets/music/I Don't Like - Chief Keef.mp3",
-      "assets/music/In Control - Youngboy Never Broke Again.mp3",
-      "assets/music/Love Sosa - Chief Keef.mp3",
-      "assets/music/M.A.A.D. City - Kendrick Lamar.mp3",
-      "assets/music/Make No Sense - Youngboy Never Broke Again.mp3",
-      "assets/music/Monster - Kanye West.mp3",
-      "assets/music/Only You Freestyle - Headie One.mp3",
-      "assets/music/Sawarim - idk the author.mp3",
-      "assets/music/Sosa - Chief Keef.mp3",
-      "assets/music/Spinback - Comethazine.mp3",
-      "assets/music/Still Ballin' (Remix) - 2Pac.mp3",
-      "assets/music/True Religion Fein - Chief Keef.mp3",
-      "assets/music/Walk - Comethanize.mp3",
-      "assets/music/Wesley's Theory - Kendrick Lamar.mp3"
-    ];
-    var audio = new Audio();
-    audio.loop = true;
-    audio.preload = "auto";
-    var currentSongIndex = -1;
-    
-    function playNextSong() {
-      var nextSongIndex = Math.floor(Math.random() * songs.length);
-      while (nextSongIndex === currentSongIndex) {
-        nextSongIndex = Math.floor(Math.random() * songs.length);
-      }
-      currentSongIndex = nextSongIndex;
-      var nextSong = songs[currentSongIndex];
-      audio.src = nextSong;
-    
-      var nowPlayingTitle = document.querySelector(".now-playing-title");
-      if (nowPlayingTitle) {
-        nowPlayingTitle.innerHTML = "Now Playing: " + (currentSongIndex + 1) + ":";
-      }
-    
-      var nowPlayingSong = document.querySelector(".now-playing-song");
-      if (nowPlayingSong) {
-        nowPlayingSong.innerHTML = songs[currentSongIndex];
-      }
-    }        
-    
-    audio.addEventListener("ended", playNextSong);
-    playNextSong();
+var songs = [
+  "assets/music/Cant Trust - Keef Chief.mp3",
+  "assets/music/Control - Ab-Soul.mp3",
+  "assets/music/DNA - Kendrick Lamar.mp3",
+  "assets/music/Everyday - Chief Keef.mp3",
+  "assets/music/Geeked (Bass Boosted) - CHII WVTTZ + Set Da Trend + C Blu + Dougie B.mp3",
+  "assets/music/Get Out - Comethazine.mp3",
+  "assets/music/Go Stupid - Polo G.mp3",
+  "assets/music/I Don't Like - Chief Keef.mp3",
+  "assets/music/In Control - Youngboy Never Broke Again.mp3",
+  "assets/music/Love Sosa - Chief Keef.mp3",
+  "assets/music/M.A.A.D. City - Kendrick Lamar.mp3",
+  "assets/music/Make No Sense - Youngboy Never Broke Again.mp3",
+  "assets/music/Monster - Kanye West.mp3",
+  "assets/music/Only You Freestyle - Headie One.mp3",
+  "assets/music/Sawarim - idk the author.mp3",
+  "assets/music/Sosa - Chief Keef.mp3",
+  "assets/music/Spinback - Comethazine.mp3",
+  "assets/music/Still Ballin' (Remix) - 2Pac.mp3",
+  "assets/music/True Religion Fein - Chief Keef.mp3",
+  "assets/music/Walk - Comethanize.mp3",
+  "assets/music/Wesley's Theory - Kendrick Lamar.mp3"
+];
+var audio = new Audio();
+audio.loop = true;
+audio.preload = "auto";
+var currentSongIndex = -1;
+document.addEventListener("DOMContentLoaded", function() {
+  function playNextSong() {
+    var nextSongIndex = Math.floor(Math.random() * songs.length);
+    while (nextSongIndex === currentSongIndex) {
+      nextSongIndex = Math.floor(Math.random() * songs.length);
+    }
+    currentSongIndex = nextSongIndex;
+    var nextSong = songs[currentSongIndex];
+    audio.src = nextSong;
+    console.log(document.querySelector(".now-playing-title"));
+  
+    var nowPlayingTitle = document.querySelector(".now-playing-title");
+    if (nowPlayingTitle) {
+      nowPlayingTitle.innerHTML = "Now Playing:";
+    }
+  
+    console.log(document.querySelector(".now-playing-song"));
+  
+    var nowPlayingSong = document.querySelector(".now-playing-song");
+    if (nowPlayingSong) {
+      nowPlayingSong.innerHTML = nextSong.replace(/^.*[\\\/]/, '').slice(0, -4);
+    }
+  }          
+  
+  audio.addEventListener("ended", playNextSong);
+  playNextSong();
 
 window.addEventListener("load", function() {
-  audio.play();
+audio.play();
+}); 
 });
