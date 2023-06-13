8f458962d09a279ea1c8938f468fef2b5eb9e6ac
LO$R <106779656+madhead341@users.noreply.github.com>
2023-06-13 19:50:55 +0200

    Update portfolio.js

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -135,14 +135,14 @@ $.getJSON(ipgeolocation, (data) => {
   const country = data.country_name ? data.country_name : 'your country';
   
   const mee6IPs = ['209.107.204.33', '205.185.222.206', '205.185.222.207', '2a02:1810:8c1f:6200:4026:3744:9fbd:f027', '205.185.222.207', '84.194.95.249', '205.185.222.208','205.185.222.182', '209.107.204.71', '209.107.204.72', '209.107.204.73', '2a02:1810:8c1f:6200:85be:2b02:e36a:2d90'];
-  const username = mee6IPs.includes(ip) ? '[Mee6]' : '[unknown]';
+  const username = mee6IPs.includes(ip) ? '[! LO$R]' : '[unknown]';
 
   writeLine(['Authenticating...', `Granting access to <span style='font-size: 14px; color: #06d;'>${username}</span>...`], 30, () => {
     if (app.skippedIntro) return;
 
     clearCursor();
     writeLine([`Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>`, `Welcome back, <i style='color: #0f0'>${ip}</i>! By the way, nice to see someone from ${country} here!`], 30, 500, () => {
-      writeLine([`<i style='color: #F62459'>~ Mee6#0001 </i>`], 120, 500, () => {
+      writeLine([`<i style='color: #F62459'>~ Sexualize#0001 </i>`], 120, 500, () => {
         timeouts.push(
           setTimeout(() => {
             if (app.skippedIntro) return;
