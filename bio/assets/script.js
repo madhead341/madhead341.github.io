ebe904c161640aeedc3498a05050b4da4849532c
! LO$R <106779656+madhead341@users.noreply.github.com>
2025-06-29 18:11:49 +0200

    Update script.js

--- bio/assets/script.js
+++ bio/assets/script.js
@@ -1,4 +1,11 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+document.addEventListener('keydown', function(event) {
+  if (event.ctrlKey) {
+    event.preventDefault();
+  }
+});
+
 function setupTooltip(link, address, message) {
     link.classList.add('tooltip-container');
 
